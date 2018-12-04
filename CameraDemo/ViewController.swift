e38c6d7eedc2b22ce278e0ab9c3e045e195b4884
GERARDO DEHUMA <rl200066@rlas-macair-23.rlas-116.org>
2018-12-04 10:27:06 -0600

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by GERARDO DEHUMA on 12/4/18.
+//  Copyright © 2018 GERARDO DEHUMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
